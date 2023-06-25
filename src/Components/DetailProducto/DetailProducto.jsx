75f289f317c38e45a5591f712cacecbe600e1201
Akadus23 <davilgo23@gmail.com>
2023-06-25 12:58:25 -0500

    .

--- src/Components/DetailProducto/DetailProducto.jsx
+++ src/Components/DetailProducto/DetailProducto.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom'
+import NavBar from '../NavBar/NavBar';
 
 //Aquí se renderiza el detalle de cada producto
 
 export default function DetailProducto() {
+
+  let {id} = useParams();
+  const dispatch = useDispatch();
+  const productDetails = useSelector((state) => state.details);
+
   return (
-    <div>DetailProducto</div>
+    <div>
+      <div className='w-full'>
+        <NavBar />
+      </div>
+      <div className='flex w-full flex bg-gray-300'>
+      </div>
+    </div>
   )
 }
