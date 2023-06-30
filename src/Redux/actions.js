24aad8daae2c155e7a6ce79bf37d291a5ac33524
caparada1366 <ca.parada1366@gmail.com>
2023-06-30 13:43:16 -0500

    Crear, get y detail conectados

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -17,6 +17,21 @@ export function getAllProducts(){
     }
 }
 
+
+export function getAllCategorias(){
+    const linkFelipe = `https://commerce-back-2025.up.railway.app/categorias`
+    const linkBackLocal= 'http://localhost:3001/productos/productos'
+    return async (dispatch)=>{
+        const data = (await axios.get(linkFelipe)).data;
+        //.content;           //para el local
+       //para el deploy
+        return dispatch({
+            type: 'GET_ALL_CATEGS',
+            payload: data
+        });
+    }
+}
+
 export function getDetail(id){
     return async function(dispatch){
         const json = await axios(`http://localhost:3000/productos/${id}`);
